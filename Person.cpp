37e11a6670b2bf9e33a123fe8e69a695217f97e9
Ha Thanh Dat - Thinkpad P50 <htdat2711@gmail.com>
2023-10-21 02:49:14 +0700

    soucre GPTB@ OOP comment

--- Person.cpp
+++ Person.cpp
@@ -1,41 +1,42 @@
-#include<iostream>
-#include<cmath>
+#include<iostream> // khai báo thư viện iostream
+#include<math.h>// khai báo thư viện toán math.h 
+using namespace std;
 
-class QuadraticEquation {
+class QuadraticEquation {   // Định nghĩa một lớp tên là QuadraticEquation
 private:
-    double a, b, c;
+    double a, b, c;  // khai báo biến dưới dạng double và thành viên của biến đó là a,b,c
 public:
-    void input() {
-        std::cout << "Enter coefficient a: ";
-        std::cin >> a;
-        std::cout << "Enter coefficient b: ";
-        std::cin >> b;
-        std::cout << "Enter coefficient c: ";
-        std::cin >> c;
+    void input() { // cho phép người dùng nhập dữ liệu vào các biến a,b,c
+        cout << "Moi nhap he so a: ";
+        cin >> a;
+        cout << "Moi nhap he so b: ";
+        cin >> b;
+        cout << "Moi nhap he so c: ";
+        cin >> c;
     }
 
-    void solve() {
+    void solve() { // giai phuong trinh bac 2
         if (a == 0) {
-            std::cout << "This is not a quadratic equation.\n";
+            cout << "Day khong phai la phuong trinh bac 2";
         } else {
             double delta = b * b - 4 * a * c;
             if (delta < 0) {
-                std::cout << "The equation has no real roots.\n";
+                cout << "Phuong trinh vo nghiem";
             } else if (delta == 0) {
                 double x = -b / (2 * a);
-                std::cout << "The equation has one root: " << x << "\n";
+                cout << "Phuong trinh co nghiem kep " << x <<endl;
             } else {
                 double x1 = (-b + sqrt(delta)) / (2 * a);
                 double x2 = (-b - sqrt(delta)) / (2 * a);
-                std::cout << "The equation has two roots: " << x1 << " and " << x2 << "\n";
+                cout << "phuong trinh co 2 nghiem phan biet " << x1 << " va " << x2 << endl;
             }
         }
     }
 };
 
-int main() {
+int main() {  // tao một đối tượng lớp giải phương tình bậc 2
     QuadraticEquation eq;
-    eq.input();
-    eq.solve();
+    eq.input();  // để nhập hệ số biến a,b,c
+    eq.solve();  // để giải phương trình bậc 2
     return 0;
 }
