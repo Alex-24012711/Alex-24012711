25111f8e8de0810b2cd554289330322f7331aa8f
Ha Thanh Dat - Thinkpad P50 <htdat2711@gmail.com>
2023-10-21 01:07:57 +0700

    source message giaiptbac2

--- Person.cpp
+++ Person.cpp
@@ -1,7 +1,27 @@
 #include<iostream>
+#include<math.h>5
+
 using namespace std;
 
 int main(){
-  cout<<"Hello world";
+  float a,b,c,x,x1,x2,delta;
+  cin>>a>>b>>c;
+  delta=(b*b)-(4*a*c);
+  if(delta<0){
+    cout<<"Phuong trinh vo nghiem"<<endl;
+  }
+  else if(delta==0){
+    x=(-b)/(2*a);
+     cout<<"Phuong trinh co nghiem kep"<<endl;
+  }
+  else {
+    delta=sqrt(delta);
+    cout<<"Phuong trinh co 2 nghiem phan biet" <<endl;
+    x1 = (-b + delta) / (2*a);
+		x2 = (-b - delta) / (2*a);
+    cout<<"x1="<<x1<<endl;
+    cout<<"x2="<<x2<<endl;
+  }
   return 0;
+
 }
