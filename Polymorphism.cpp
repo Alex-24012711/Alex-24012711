6fe897fcc493f924184a12e8a3de4e37e2479b56
tên account <Email ca bn>
2023-10-22 01:47:42 +0700

    cmtPolymorphism,.cpp

--- Polymorphism.cpp
+++ Polymorphism.cpp
@@ -1,20 +1,20 @@
-#include<iostream>
-using namespace std;
-
-class Human{
+#include<iostream>  // khai báo thư viện chuẩn iostream
+using namespace std;  // khai báo sử dụng hàm std
+// đoạn code sau thể hiện tính kế thừa và tính đa hình 
+class Human{   // khai báo lớp Human
     public:
-    void speak(){
+    void speak(){ // khai báo hàm void không trả về giá trị gì
         cout<<"Human is speak"<<endl;
     }
 
 };
-class American:public Human{
+class American:public Human{ // lớp American kế thừa lớp cha Human
     public:
     void speak(){
         cout<<"American is speak english"<<endl;
     }
 };
-class Vietnamese:public Human{
+class Vietnamese:public Human{ // lớp VietNamese kế thừa lớp cha Human
     public:
     void speak(){
         cout<<"Vietnamese is speak vietnam language"<<endl;
