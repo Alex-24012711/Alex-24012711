326f0abbe133c2d872716202bba82f073d1b6295
tên account <Email ca bn>
2023-10-22 16:16:26 +0700

    PolymorphismTeacher.cpp

--- Polymorphism.cpp
+++ Polymorphism.cpp
@@ -1,22 +1,37 @@
-#include<iostream>  // khai báo thư viện chuẩn iostream
-using namespace std;  // khai báo sử dụng hàm std
-// đoạn code sau thể hiện tính kế thừa và tính đa hình 
-class Human{   // khai báo lớp Human
-    public:
-    void speak(){ // khai báo hàm void không trả về giá trị gì
-        cout<<"Human is speak"<<endl;
-    }
+#include <iostream>
 
+// Lớp cơ sở
+class Teacher {
+public:
+    virtual void teach() = 0;  // Phương thức ảo
 };
-class American:public Human{ // lớp American kế thừa lớp cha Human
-    public:
-    void speak(){
-        cout<<"American is speak english"<<endl;
+
+// Lớp dẫn xuất: MathTeacher
+class MathTeacher : public Teacher {
+public:
+    void teach() override {
+        std::cout << "Teaching Math" << std::endl;
     }
 };
-class Vietnamese:public Human{ // lớp VietNamese kế thừa lớp cha Human
-    public:
-    void speak(){
-        cout<<"Vietnamese is speak vietnam language"<<endl;
+
+// Lớp dẫn xuất: LiteratureTeacher
+class LiteratureTeacher : public Teacher {
+public:
+    void teach() override {
+        std::cout << "Teaching Literature" << std::endl;
     }
 };
+
+// Sử dụng tính đa hình
+int main() {
+    MathTeacher mathTeacher;
+    LiteratureTeacher literatureTeacher;
+
+    Teacher* teacher = &mathTeacher;
+    teacher->teach();  // In ra: "Teaching Math"
+
+    teacher = &literatureTeacher;
+    teacher->teach();  // In ra: "Teaching Literature"
+
+    return 0;
+}
