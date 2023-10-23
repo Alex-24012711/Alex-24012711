253b96664251514a84113f54dbe7fc4246c520e6
tên account <Email ca bn>
2023-10-24 01:00:40 +0700

    DataabstractiongOOPcmt.cpp

---
+++ DataabstractionOOP.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+// lớp trừu tượng cho phép chúng ta tạo ra các lớp hoặc các đối tượng mà chỉ hiện thị các thông tin và hành vi cần thiết đối với người dùng và ẩn hết các chi tiết triển khai bên trong 
+// Tính trừu tượng để lập trình viên có thể ẩn tất cả dữ liệu hoặc quá trình không liên quan của ứng dụng
+// Vì Lớp trừu tượng giúp cho giao diện người dùng đơn giản hơn, ẩn các đoạn mã code phức tạp, thuận tiện cho việc bảo trì và cập nhập phần mềm dễ dàng hơn 
+
+
+class Animal{ // lớp trừu tượng 
+    virtual void phatAm()=0;
+};
+class Dog: public Animal{
+    public: 
+     void phatAm(){
+        cout<<"Gau gau"<<endl;
+     }
+};                                   // Lớp trừu tượng sẽ ẩn hết các lớp con Dog và cat và chỉ hiện thị tiếng con con chó và con mèo kêu
+class Cat: public Animal{
+    public: 
+    void phatAm(){
+        cout<<"Meow"<<endl;
+    }
+};
+int main(){
+    Dog dog;
+    Cat cat;
+    dog.phatAm(); // Gọi phương thức trừu tượng
+    cat.phatAm();
+    return 0;
+}
--- Polymorphism.cpp
+++ Polymorphism.cpp
@@ -1,37 +1,94 @@
-#include <iostream>
+#include<iostream>
+using namespace std;
 
-// Lớp cơ sở
-class Teacher {
-public:
-    virtual void teach() = 0;  // Phương thức ảo
+class NhanVien{
+    protected:
+    string hoTen;
+    float luong;
+    public:
+    NhanVien()
+    {
+        this->hoTen ="";
+        this->luong=0.0;
+    }
+    void nhap(){
+        cout<<"Ho ten: ";
+        cin.ignore();
+        getline(cin, this->hoTen);
+    }
+    void xuat()
+    {
+        cout<<"Ho ten:";
+        cout<<this->hoTen<<endl;
+    }
+    virtual void tinhLuong()=0;
 };
+class NhanVienSanXuat : public NhanVien
+{
+    private:
+    int soSanPham;
+    float tienCong1SP;
+    public:
+    NhanVienSanXuat(): NhanVien()
+    {
+        this->soSanPham=0;
+        this->tienCong1SP=0;
+    }
+    void nhap(){
+        NhanVien::nhap();
+        cout<<"So san pham:";
+        cin>> this->soSanPham;
+        cout<<"Tien cong 1 san pham: ";
+        cin>> this->tienCong1SP;
+    }
+    void xuat(){
+        NhanVien::xuat();
+        cout<<"So san pham: ";
+        cout<< this->soSanPham <<endl;
+        cout<<"Tien con 1 san pham:"<<endl;
+        cout<<this->tienCong1SP<<endl;
+        cout<<"Luong: ";
+        cout<<this->luong<<endl;
+    }
+    void tinhLuong(){
+        this->luong=this->soSanPham*this->tienCong1SP;
+    }
 
-// Lớp dẫn xuất: MathTeacher
-class MathTeacher : public Teacher {
+};
+class NhanVienVanPhong : public NhanVien
+{
+private:
+    float luongCoBan;
+    int soNgayLamViec;
+
 public:
-    void teach() override {
-        std::cout << "Teaching Math" << std::endl;
+    NhanVienVanPhong() : NhanVien()
+    {
+        this->luongCoBan = 0.0;
+        this->soNgayLamViec = 0;
+    }
+    void nhap()
+    {
+        NhanVien::nhap();
+        cout << "Luong co ban: ";
+        cin >> this->luongCoBan;
+        cout << "So ngay lam viec: ";
+        cin >> this->soNgayLamViec;
+    }
+
+    void xuat()
+    {
+        NhanVien::xuat();
+        cout << "Luong co ban: ";
+        cout << this->luongCoBan << endl;
+        cout << "So ngay lam viec: ";
+        cout << this->soNgayLamViec << endl;
+        cout << "Luong: ";
+        cout << this->luong << endl;
+    }
+
+    void tinhLuong()
+    {
+        this->luong = this->soNgayLamViec * this->luongCoBan;
     }
 };
-
-// Lớp dẫn xuất: LiteratureTeacher
-class LiteratureTeacher : public Teacher {
-public:
-    void teach() override {
-        std::cout << "Teaching Literature" << std::endl;
-    }
-};
-
-// Sử dụng tính đa hình
-int main() {
-    MathTeacher mathTeacher;
-    LiteratureTeacher literatureTeacher;
-
-    Teacher* teacher = &mathTeacher;
-    teacher->teach();  // In ra: "Teaching Math"
-
-    teacher = &literatureTeacher;
-    teacher->teach();  // In ra: "Teaching Literature"
-
-    return 0;
-}
